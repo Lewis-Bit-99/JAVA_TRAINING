c3d3087adb01143d89c7bdf27e15d5d694cd9587
Lewis-Bit-99 <lennoxlewis670@gmail.com>
2024-11-18 11:30:18 +0800

    The Codes of the day

---
+++ SecondCode.java
@@ -0,0 +1,9 @@
+public class SecondCode {
+    
+     public static void main(String[] args) {
+        
+         System.out.println("Second Code Of The days");
+     }
+
+
+}
--- firstcode.java
+++ firstcode.java
@@ -1,3 +1,9 @@
 public class firstcode {
     
+
+   public static void main(String[] args) {
+
+         System.out.println("Java Classroom");
+    
+   }
 }
